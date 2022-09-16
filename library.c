f63e094b1dc1ff48a112fad558f3cc4512f16ca6
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2022-09-16 19:45:25 +0600

    added bookList function

--- library.c
+++ library.c
@@ -91,3 +91,16 @@ void addBook(){
     fwrite(&b, sizeof(b), 1, fp);
     fclose(fp);
 }
+void booksList(){
+
+    system("cls");
+    printf("<== Available Books ==>\n\n");
+    printf("%-10s %-30s %-20s %s\n\n", "Book id", "Book Name", "Author", "Date");
+
+    fp = fopen("books.txt", "rb");
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %s\n", b.id, b.bookName, b.authorName, b.date);
+    }
+
+    fclose(fp);
+}
