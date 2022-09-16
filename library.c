a3b94cd9a8a1196e0380ac12905bf6c2f9c6a9a1
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2022-09-16 19:42:10 +0600

    Updated main function

--- library.c
+++ library.c
@@ -16,3 +16,53 @@ struct student{
     char date[12];
 }s;
 FILE *fp;
+int main(){
+
+    int ch;
+
+    while(1){
+        system("cls");
+        printf("<== Library Management System ==>\n");
+        printf("1.Add Book\n");
+        printf("2.Books List\n");
+        printf("3.Remove Book\n");
+        printf("4.Issue Book\n");
+        printf("5.Issued Book List\n");
+        printf("0.Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch(ch){
+        case 0:
+            exit(0);
+
+        case 1:
+            addBook();
+            break;
+
+        case 2:
+            booksList();
+            break;
+
+        case 3:
+            del();
+            break;
+
+        case 4:
+            issueBook();
+            break;
+
+        case 5:
+            issueList();
+            break;
+
+        default:
+            printf("Invalid Choice...\n\n");
+
+        }
+        printf("Press Any Key To Continue...");
+        getch();
+    }
+
+    return 0;
+}
