96d56440ab6a62a7b7393735f0d6e95e0c88b91b
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2022-09-16 19:42:45 +0600

    added addBook function

--- library.c
+++ library.c
@@ -66,3 +66,28 @@ int main(){
 
     return 0;
 }
+void addBook(){
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(b.date, myDate);
+
+    fp = fopen("books.txt", "ab");
+
+    printf("Enter book id: ");
+    scanf("%d", &b.id);
+
+    printf("Enter book name: ");
+    fflush(stdin);
+    gets(b.bookName);
+
+    printf("Enter author name: ");
+    fflush(stdin);
+    gets(b.authorName);
+
+    printf("Book Added Successfully");
+
+    fwrite(&b, sizeof(b), 1, fp);
+    fclose(fp);
+}
