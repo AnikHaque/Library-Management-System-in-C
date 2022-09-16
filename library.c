94ca6eb2f0717cf4fda1c346f4445382ac2d9ef0
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2022-09-16 19:47:07 +0600

    Added issueBook functionality

--- library.c
+++ library.c
@@ -137,3 +137,54 @@ void del(){
     rename("temp.txt", "books.txt");
 
 }
+void issueBook(){
+
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(s.date, myDate);
+
+    int f=0;
+
+    system("cls");
+    printf("<== Issue Books ==>\n\n");
+
+    printf("Enter Book id to issue: ");
+    scanf("%d", &s.id);
+
+    //Check if we have book of given id
+    fp = fopen("books.txt", "rb");
+
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        if(b.id == s.id){
+            strcpy(s.bookName, b.bookName);
+            f=1;
+            break;
+        }
+    }
+
+    if(f==0){
+        printf("No book found with this id\n");
+        printf("Please try again...\n\n");
+        return;
+    }
+
+    fp = fopen("issue.txt", "ab");
+
+    printf("Enter Student Name: ");
+    fflush(stdin);
+    gets(s.sName);
+
+    printf("Enter Student Class: ");
+    fflush(stdin);
+    gets(s.sClass);
+
+    printf("Enter Student Roll: ");
+    scanf("%d", &s.sRoll);
+
+    printf("Book Issued Successfully\n\n");
+
+    fwrite(&s, sizeof(s), 1, fp);
+    fclose(fp);
+}
