e2a7b4cb0bfb2c7012437f9f734ce87764fcae91
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2022-09-16 19:47:52 +0600

    Added issueList functionality

--- library.c
+++ library.c
@@ -188,3 +188,16 @@ void issueBook(){
     fwrite(&s, sizeof(s), 1, fp);
     fclose(fp);
 }
+void issueList(){
+    system("cls");
+    printf("<== Book Issue List ==>\n\n");
+
+    printf("%-10s %-30s %-20s %-10s %-30s %s\n\n", "S.id", "Name", "Class", "Roll", "Book Name", "Date");
+
+    fp = fopen("issue.txt", "rb");
+    while(fread(&s, sizeof(s), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %-10d %-30s %s\n", s.id, s.sName, s.sClass, s.sRoll, s.bookName, s.date);
+    }
+
+    fclose(fp);
+}
